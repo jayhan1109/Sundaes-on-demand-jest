460f79e94e0e2955b452883bec685f1e8d5618dd
jayhan1109 <jayhan1109@gmail.com>
2022-06-28 14:30:44 -0700

    Code and test summary form, learn user event and async await

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,5 +1,11 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("initial terms and condition checkbox and order button", () => {
   render(<SummaryForm />);
@@ -21,9 +27,34 @@ test("click checkbox enables order button and re-click disables order button", (
   });
   const orderBtn = screen.getByRole("button", { name: "Confirm order" });
 
-  fireEvent.click(termCheckbox);
+  userEvent.click(termCheckbox);
   expect(orderBtn).toBeEnabled();
 
-  fireEvent.click(termCheckbox);
+  userEvent.click(termCheckbox);
   expect(orderBtn).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const termAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termAndConditions);
+
+  const popover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  userEvent.unhover(termAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
